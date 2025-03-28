ff661aa649b598beb676911ab6e8f238a136db09
jay.panchal <jay.panchal@acldigital.com>
2025-03-28 17:52:34 +0530

    updated config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export',
+  reactStrictMode: true,
+  distDir: 'dist',
+  
 };
 
 export default nextConfig;
