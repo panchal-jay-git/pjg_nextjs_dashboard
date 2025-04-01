e454e92c0a247dfee80f9bfbdd34e3f9ab68cf0b
jay.panchal <jay.panchal@acldigital.com>
2025-04-01 13:07:12 +0530

    added dashboard page with chart and invoices details

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,56 +1,56 @@
-// // import postgres from 'postgres';
-
-// // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-// // async function listInvoices() {
-// // 	const data = await sql`
-// //     SELECT invoices.amount, customers.name
-// //     FROM invoices
-// //     JOIN customers ON invoices.customer_id = customers.id
-// //     WHERE invoices.amount = 666;
-// //   `;
-
-// // 	return data;
-// // }
-
-// export async function GET() {
-//   return Response.json({
-//     message:
-//       'Uncomment this file and remove this line. You can delete this file when you are finished.',
-//   });
-//   // try {
-//   // 	return Response.json(await listInvoices());
-//   // } catch (error) {
-//   // 	return Response.json({ error }, { status: 500 });
-//   // }
-// }
-// app/query/route.js
 import postgres from 'postgres';
 
-const sql = postgres('', { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!,{ssl:'verify-full'});
 
 async function listInvoices() {
-  const data = await sql`
+	const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
   `;
-  return data;
+
+	return data;
 }
 
 export async function GET() {
-  try {
-    const data = await listInvoices();
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error }), {
-      status: 500,
-    });
-  }
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
+  // try {
+  // 	return Response.json(await listInvoices());
+  // } catch (error) {
+  // 	return Response.json({ error }, { status: 500 });
+  // }
 }
+// app/query/route.js
+// import postgres from 'postgres';
+
+// const sql = postgres('', { ssl: 'require' });
+
+// async function listInvoices() {
+//   const data = await sql`
+//     SELECT invoices.amount, customers.name
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE invoices.amount = 666;
+//   `;
+//   return data;
+// }
+
+// export async function GET() {
+//   try {
+//     const data = await listInvoices();
+//     return new Response(JSON.stringify(data), {
+//       status: 200,
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: error }), {
+//       status: 500,
+//     });
+//   }
+// }
